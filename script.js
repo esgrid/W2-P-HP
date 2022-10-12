dc5063261a4a22b9a87af69202cb6d7a52caf717
Esgrid Sikahall <esikahall@gmail.com>
2022-10-12 23:04:27 +0100

    got rid of global event listener, too much for a workshop

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 import characters from "./data.js";
 
-// function addGlobalEventListener(type, selector, callback, options){
-//     document.addEventListener(type, e => {
-//         if (e.target.matches(selector)) callback(e)
-//     }, options)
-// }
-
 const main = document.querySelector('.main')
 
-let createCharacter = function ({name, image}) {
+function createCharacter ({name, image}) {
     const card = document.createElement('div')
     card.classList.add('card')
 
@@ -30,10 +24,7 @@ let createCharacter = function ({name, image}) {
         `
     }
 
-    const sliders = ["wind", "fire", "earth", "water"]
-    sliders.forEach( slider => {
-        sliderContainer.innerHTML += createSlider(slider)
-    })
+    ["wind", "fire", "earth", "water"].forEach( slider => sliderContainer.innerHTML += createSlider(slider))
 
     const sortingButton = document.createElement('button')
     sortingButton.append("Go to...")
@@ -71,26 +62,4 @@ document.addEventListener('click', e => {
         }
     }    
 })
-
-// addGlobalEventListener('click', 'button', (e) => {
-//     const card = e.target.closest('.card')
-//     const inputs = card.querySelectorAll('input')
-
-//     let chosenIndex = Math.round(Math.random() * 3)
-//     let maxElement = inputs[chosenIndex].value;
-
-//     for (let index = 0; index < inputs.length; index++) {
-//         const newMaxElement = inputs[index].value
-//         if (maxElement < newMaxElement) {
-//             maxElement = newMaxElement
-//             chosenIndex = index
-//         }
-//     }
-
-//     if (card.classList.length > 1){
-//         card.classList.replace(card.classList[1], inputs[chosenIndex].className)
-//     } else{
-//         card.classList.toggle(inputs[chosenIndex].className) 
-//     }
-// }, null)
 
